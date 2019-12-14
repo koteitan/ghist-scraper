2a5abaf3ca7e498a58adfe0d1a902464de1a0ab2
koteitan <ukaibutton@mac.com>
2019-12-13 23:06:49 -0500

    sort as the older is the earlier/git commit

--- scrap.bash
+++ scrap.bash
@@ -2,18 +2,26 @@
 
 URL=$1
 URLDIR=`echo $1 | sed "s/\(https:\/\/\)\([^/]*\)\(.*$\)/\2/g"`
+mkdir work
+cd work
 
 wget "$1?limit=500&action=history" -O list.html
 
-tail -r list.html \
-  | grep mw-history-histlinks \
-  | sed "s/\
-\(^[^f]*href=\"\)\
-\([^\"]*oldid\=\)\
-\([^\"]*\)\
-\(.*value\=\"\)\
-\([^\"]*\)\(.*$\)\
-/wget \"https:\/\/$URLDIR\2\3\" \-O \3.html/g" > download.bash
+git init
 
-#bash download.bash
+tail -r list.html \
+| grep mw-history-histlinks \
+| sed "s/\
+\(^.*name=\"diff[^e]*href=\"\)\
+\([^\"]*oldid=\)\
+\([^\"]*\)\
+\(\"[^>]*>\)\
+\([^<]*\)\
+\(.*$\)\
+/wget \"https:\/\/$URLDIR\2\3\" \-O body.html \&\& \
+git add body.html \&\& \
+git commit -m\"\5\"\
+/g" > download.bash
+
+bash download.bash
 
